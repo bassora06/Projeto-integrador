849d06c0bc96944c5389705ddeffb13b6f6cdc6a
João Ambrósio <jgadgaambrosio@gmail.com>
2025-03-08 11:13:38 -0300

    Subindo o projeto Spring criado no Spring Initializr

---
+++ src/main/java/fatec/pi/rod/onbus/OnbusApplication.java
@@ -0,0 +1,13 @@
+package fatec.pi.rod.onbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fatec/pi/rod/onbus/OnbusApplicationTests.java
@@ -0,0 +1,13 @@
+package fatec.pi.rod.onbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
