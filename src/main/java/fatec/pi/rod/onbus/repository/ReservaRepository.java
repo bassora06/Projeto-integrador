ac0b99e3eac7515341b61f9dec91c7290025d947
Lucas Bassora da Silva <116023835+bassora06@users.noreply.github.com>
2025-10-07 19:44:39 -0300

    Delete src directory

--- src/main/java/fatec/pi/rod/onbus/repository/ReservaRepository.java
+++
@@ -1,7 +0,0 @@
-package fatec.pi.rod.onbus.repository;
-
-import fatec.pi.rod.onbus.entity.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
-}
